5e01f81c2ca8628cdefc1b8f9f0bc5f7b1a437f8
sshuman95 <49686103+sshuman95@users.noreply.github.com>
2019-07-02 15:23:43 -0700

    test9900000

--- Backend/server.js
+++ Backend/server.js
@@ -26,7 +26,7 @@ if(process.env.NODE_ENV ==='production'){
     })
 }
 
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 
 app.listen(port,()=>console.log(`Server started on port ${port}`));
 
