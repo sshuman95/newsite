57e7d4f4db698594acf005ec421526571f610d0b
sshuman95 <49686103+sshuman95@users.noreply.github.com>
2019-07-02 16:36:45 -0700

    Please!!5699

--- Backend/server.js
+++ Backend/server.js
@@ -6,13 +6,15 @@ const comments = require('./routes/api/comments');
 const emails = require('./routes/api/email');
 const path = require('path');
 const dotenv = require('dotenv');
+dotenv.config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+let mongo_uri = 'mongodb+srv://Buster:Upstreambjj@cluster0-cwzms.mongodb.net/test?retryWrites=true&w=majority'
 
 mongoose
-.connect('mongodb+srv://Buster:Upstreambjj@cluster0-cwzms.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true })
+.connect(process.env.mongo_uri,{ useNewUrlParser: true })
 .then(()=>console.log('MongoDB Connected'))
 .catch(err=>console.log(err));
 
