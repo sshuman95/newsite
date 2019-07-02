db5bd6d9c3cecd1d6a3d64ea08d3861308c72981
sshuman95 <49686103+sshuman95@users.noreply.github.com>
2019-07-02 14:46:33 -0700

    first32

--- Backend/server.js
+++ Backend/server.js
@@ -19,7 +19,7 @@ app.use('/api/comments',comments)
 app.use('/api/email',emails)
 
 if(process.env.NODE_ENV ==='production'){
-    app.use(express.static('app/build'));
+    app.use(express.static('newsite/build'));
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(__dirname,'app','build','index.html'))
     })
