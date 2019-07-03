de1cb2def9f01e84d433c82b03bad2a4997229aa
sshuman95 <49686103+sshuman95@users.noreply.github.com>
2019-07-03 09:25:34 -0700

    64549

--- Backend/server.js
+++ Backend/server.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === "production"){
     // express will serve index.html if it doesnt recognize route
     const path = require('path');
     app.get("/", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname,"Backend", 'build', 'index.html'));
     })
 }
 
