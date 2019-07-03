f69e1a41adb66732ffdaf2ebc02908ee7ba32ff0
sshuman95 <49686103+sshuman95@users.noreply.github.com>
2019-07-03 06:10:34 -0700

    jfkdslf3323524

--- Backend/server.js
+++ Backend/server.js
@@ -23,12 +23,12 @@ app.use('/api/email',emails)
 if (process.env.NODE_ENV === "production"){
     // express will serve production assets ( main.js, main.css )
     // look inside client/build to serve assets
-    app.use(express.static('client/build'));
+    app.use(express.static('build'));
 
     // express will serve index.html if it doesnt recognize route
     const path = require('path');
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     })
 }
 
