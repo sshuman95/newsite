02698d0010ac51078af2150607a77944813eb55b
sshuman95 <49686103+sshuman95@users.noreply.github.com>
2019-07-03 15:08:17 -0700

    please please!!!

--- server.js
+++ server.js
@@ -1,29 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const app = express();
-const comments = require('./routes/api/comments');
-const emails = require('./routes/api/email');
-const dotenv = require('dotenv');
-const keys = require("./config/keys");
 
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./routes/routes");
+const dotenv = require("dotenv");
+const keys = require("./config/keys");
+const app = express();
 dotenv.config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
 mongoose
 	.connect(
+		// I'm not completely sure what is what, but these DO NOT belong in source control
+		// if they are keys unique to your database.
 		keys.mongoURI,
 		{ useNewUrlParser: true }
 	)
 	.then(() => console.log("MongoDB Connected"))
 	.catch(err => console.log(err));
 
-app.use('/api/comments',comments)
-app.use('/api/email',emails)
-
+routes(app);
 
 if (process.env.NODE_ENV !== "production") {
 	// express will serve production assets ( main.js, main.css )
@@ -39,4 +37,4 @@ if (process.env.NODE_ENV !== "production") {
 
 const port = process.env.PORT || 8080;
 
-app.listen(port,()=>console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
