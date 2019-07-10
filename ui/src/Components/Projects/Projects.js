ea0a7c79a303d5d6092ca1cd515070b58138c951
sshuman95 <49686103+sshuman95@users.noreply.github.com>
2019-07-10 11:57:22 -0700

    chaning portfolio

--- ui/src/Components/Projects/Projects.js
+++ ui/src/Components/Projects/Projects.js
@@ -2,29 +2,52 @@ import React from 'react';
 import './projects.css';
 
 
-let ravenous = (<div className='ravenous'>
+let ravenous = (
+<div className='project'>
+  <div>
 <h1>Ravenous</h1>
+<img src='https://i.imgur.com/0oUC7As.png' alt='Ravenous Page Example' className='siteExample'/>
+</div>
+<div className="about">
 <h3>About</h3>
 <p>This project was built in React and uses the Yelp API to fetch data, including location, ratings, and reviews.<br/> It can be queried using 'Best Match', 'Highest Rated', or 'Most Reviewed'</p>
-<a style={{color:'white'}} href='https://wrong-cherries.surge.sh'>Click Here To Visit</a>
+</div>
+<div className='buttons'>
+<a style={{color:'black'}} href='https://wrong-cherries.surge.sh'>View Site</a>
+<a style={{color:'black'}} href='https://github.com/sshuman95/ravenous'>View Code</a>
+</div>
 </div>)
 
-let upstream = (<div className='upstream'>
-<h1>Upstream Bjj</h1>
+let upstream = (
+<div className='project'>
+  <div>
+  <h1>Upstream Bjj</h1>
+<img src='https://i.imgur.com/kGYsrFd.png' alt='Upstream Page Example' className='siteExample'/>
+</div>
+<div className="about">
 <h3>About</h3>
 <p>This project is a front-end application that was built in React and uses the Stripe API to complete orders.<br/>
 It will be used to sell the gym's merchandise.</p>
-<a style={{color:'white'}} href='https://sshuman95.github.io/upstream/'>Click Here To Visit</a>
+</div>
+<div className='buttons'>
+<a style={{color:'black'}} href='https://sshuman95.github.io/upstream/'>View Site</a>
+<a style={{color:'black'}} href='https://github.com/sshuman95/upstream'>View Code</a>
+</div>
 </div>)
 
-let tea = (<div className='tea'>
-<h1>Soul Smith Kombucha</h1>
+let portfolio = (<div className='project'>
+<div>
+<h1>Portfolio</h1>
+<img src='https://i.imgur.com/1Em9ZhH.png' alt='Portfolio Page Example' className='siteExample'/>
+</div>
+<div className="about">
 <h3>About</h3>
-<p>This project was built using HTML, CSS, Javascript, and PHP. It is a static webpage for a local Tea vendor.</p>
-<a style={{color:'white'}} href='http://www.soulsmithkombucha.com/'>Click Here To Visit</a>
+<p>This site was built in React and uses MongoDB, Node, and Express.</p>
+</div>
+<div className='buttons'>
+<a style={{color:'black'}} href='https://github.com/sshuman95/newsite'>View Code</a>
+</div>
 </div>)
-
-
 
 
 class Projects extends React.Component{
@@ -32,15 +55,9 @@ class Projects extends React.Component{
     render(){
       return (
         <div id='projectContainer'>
-          <div>
             {ravenous}
-          </div>
-          <div>
             {upstream}
-            </div>
-          <div>
-            {tea}
-          </div>
+            {portfolio}
         </div>
       )
     }
